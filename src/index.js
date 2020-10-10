969fcaf446f88d500efc4a05a0e5387dd2fe0e3e
cai <flashmx29403229@gmail.com>
2020-10-10 16:28:13 +0800

    上傳

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const googleClientInit = () => {
+  gapi.load("client", () => {
+    gapi.client.init({
+      clientId: "283144508560-40ugpd9i0hj6gepplhrroihd5gaujqss.apps.googleusercontent.com",
+      scope: "profile https://www.googleapis.com/auth/user.birthday.read",
+      discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/people/v1/rest"],
+      //scope: "https://www.googleapis.com/auth/contacts.readonly",
+    });
+  });
+};
+const googleLogin = () => {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signIn().then(
+    (googleUser) => {
+      //console.log(GoogleUser);
+      console.log("Google登入成功");
+      console.log(googleUser.getBasicProfile().getImageUrl());
+      //const user_id = GoogleUser.getId();
+      //console.log(`user_id:${user_id}`);
+      //const AuthResponse = GoogleUser.getAuthResponse(true);
+      //const id_token = AuthResponse.id_token;
+      //console.log(`id_token:${id_token}`);
+      /*gapi.client.people.people.connections
+        .list({
+          resourceName: "people/me",
+          pageSize: 10,
+          personFields: "names,phoneNumbers,emailAddresses,addresses,residences,genders,birthdays,occupations",
+        })
+        .then(function (res) {
+          console.log(res);
+        });*/
+      gapi.client.people.people
+        .get({
+          resourceName: "people/me",
+          personFields: "names,phoneNumbers,emailAddresses,addresses,residences,genders,birthdays,occupations",
+        })
+        .then((res) => {
+          const str = JSON.stringify(res.result);
+          document.getElementById("content").innerHTML = str;
+        });
+    },
+    (error) => {
+      console.log("Google登入失敗");
+      console.log(error);
+    }
+  );
+};
+
+const googleDisconnect = () => {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.disconnect().then(() => {
+    console.log("User disconnect.");
+  });
+};
+const btnSignIn = document.getElementById("btnSignIn");
+btnSignIn.addEventListener("click", () => {
+  document.getElementById("content").innerHTML = "";
+  googleLogin();
+});
+const btnDisconnect = document.getElementById("btnDisconnect");
+btnDisconnect.addEventListener("click", () => {
+  googleDisconnect();
+});
+googleClientInit();
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin"); //壓縮css
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); //清除檔案資料
+const WebpackCDNInject = require('webpack-cdn-inject');
+module.exports = {
+  resolve: {
+    //擴展路徑別名
+    alias: {
+      "@fonts": path.resolve(__dirname, "./src/fonts/"),
+      "@img": path.resolve(__dirname, "./src/img/"),
+      "@css": path.resolve(__dirname, "./src/css/"),
+      "@js": path.resolve(__dirname, "./src/js/"),
+      "@src": path.resolve(__dirname, "./src/"),
+      vue: "vue/dist/vue.esm.js",
+    },
+    //擴展副檔名
+    extensions: [".js", ".json"],
+  },
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist/"),
+    filename: "js/[name].[hash].js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/"),
+    compress: true,
+    port: 9007,
+    inline: true,
+  },
+  module: {
+    rules: [
+      //css提取
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      //圖檔載入器
+      {
+        test: /\.(png|jpg|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[name].[hash].[ext]",
+            },
+          },
+        ],
+      },
+      //字型載入器
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|ttc)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[hash].[ext]",
+            },
+          },
+        ],
+      },
+      //js轉舊
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["./*"],
+    }),
+    new MiniCssExtractPlugin({ filename: "css/[name].[hash].css" }),
+    new HtmlWebpackPlugin({
+      title: "PORTFOLIO",
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new WebpackCDNInject({
+      head: ["https://apis.google.com/js/api.js"]
+    })
+  ],
+};
