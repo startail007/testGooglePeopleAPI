e8e3a09fb04e2278b0ceaea860ba9f177a539638
cai <flashmx29403229@gmail.com>
2020-10-11 23:09:24 +0800

    上傳

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+import * as firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyB5vZ8duvVnJowTvmiSSvyLyY2TOMtc-6g",
+  authDomain: "chat-292208.firebaseapp.com",
+  databaseURL: "https://chat-292208.firebaseio.com",
+  //projectId: "chat-292208",
+  storageBucket: "chat-292208.appspot.com",
+  //messagingSenderId: "344268651055",
+  //appId: "1:344268651055:web:dfd0bdd9f0e3b70f45a4da",
+  //measurementId: "G-5V70GE0KE1",
+};
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
 const userName = document.getElementById("userName");
 const userImg = document.getElementById("userImg");
 const btnSignIn = document.getElementById("btnSignIn");
@@ -20,12 +36,6 @@ const googleClientInit = () => {
         const date = data.birthdays[1].date;
         return { data, name, date };
       });
-  };
-
-  const signin = () => {
-    return getPeopleData().then((res) => {
-      userName.innerHTML = res.name;
-    });
   };
   gapi.load("client", () => {
     gapi.client
@@ -92,3 +102,61 @@ btnDisconnect.addEventListener("click", () => {
   googleDisconnect();
 });
 googleClientInit();
+
+const firebase_userName = document.getElementById("firebase_userName");
+const firebase_userImg = document.getElementById("firebase_userImg");
+const firebase_btnSignIn = document.getElementById("firebase_btnSignIn");
+const firebase_btnSignOut = document.getElementById("firebase_btnSignOut");
+
+firebase_userName.style.display = "none";
+firebase_userImg.style.display = "none";
+firebase_btnSignIn.style.display = "none";
+firebase_btnSignOut.style.display = "none";
+
+const firebase_signIn = (user) => {
+  //console.log(user);
+  firebase_userName.innerHTML = user.displayName;
+  firebase_userImg.style.backgroundImage = `url(${user.photoURL})`;
+
+  firebase_userName.style.display = "";
+  firebase_userImg.style.display = "";
+  firebase_btnSignIn.style.display = "none";
+  firebase_btnSignOut.style.display = "";
+};
+const firebase_signOut = () => {
+  //console.log(user);
+  firebase_userName.innerHTML = "";
+  firebase_userImg.style.backgroundImage = "";
+
+  firebase_userName.style.display = "none";
+  firebase_userImg.style.display = "none";
+  firebase_btnSignIn.style.display = "";
+  firebase_btnSignOut.style.display = "none";
+};
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    firebase_signIn(user);
+  } else {
+    firebase_signOut();
+  }
+});
+firebase_btnSignIn.addEventListener("click", () => {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope("profile");
+  provider.addScope("email");
+  provider.addScope("https://www.googleapis.com/auth/user.birthday.read");
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      var token = result.credential.accessToken;
+      console.log(result.credential);
+    });
+});
+
+firebase_btnSignOut.addEventListener("click", () => {
+  firebase.auth().signOut();
+  /*.then(function () {
+      alert("登出");
+    });*/
+});
